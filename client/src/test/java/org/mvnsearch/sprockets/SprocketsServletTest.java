5686e2182b5a680cf7680ebea4564c4f678059cf
linux_china <linux_china@hotmail.com>
2011-06-25 21:56:30 +0800

    sprockets servlet test

---
+++ client/src/test/java/org/mvnsearch/sprockets/SprocketsServletTest.java
@@ -0,0 +1,19 @@
+package org.mvnsearch.sprockets;
+
+import junit.framework.TestCase;
+
+/**
+ * sprockets servlet test
+ *
+ * @author linux_china
+ */
+public class SprocketsServletTest extends TestCase {
+    private SprocketsServlet sprocketsServlet = new SprocketsServlet();
+
+    /**
+     * dummy test
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
+}
